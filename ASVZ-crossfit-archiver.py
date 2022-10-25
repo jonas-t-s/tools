df521e24594ac703b1f828745d761f44387a76c0
Jonas T. Schmid <jonas.till.schmid@gmail.com>
2022-10-25 13:19:21 +0200

    Added json fetch

--- ASVZ-crossfit-archiver.py
+++ ASVZ-crossfit-archiver.py
@@ -21,13 +21,17 @@ if create_monthly_folder:
     if not os.path.exists(Folderlocation):
         os.makedirs(Folderlocation)
 if onlyday:
-    imagepath = Folderlocation + datetime.datetime.now().strftime("%d") + ".png"
+    basepath = Folderlocation + datetime.datetime.now().strftime("%d")
+    imagepath =  basepath+ ".png"
+    jsonpath = basepath + ".json"
 else:
-    imagepath = Folderlocation + datetime.datetime.now().strftime("%Y-%m-%d") + ".png"
+    basepath = Folderlocation + datetime.datetime.now().strftime("%Y-%m-%d")
+    imagepath = basepath + ".png"
+    jsonpath = basepath + ".json"
 options = selenium.webdriver.FirefoxOptions()
 options.headless = True
 browser = webdriver.Firefox(options=options)
-
+os.system("curl 'https://webwidgets.prod.btwb.com/webwidgets/wods?sections=main&track_ids=658590&activity_length=0&leaderboard_length=0&days=0' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0' -H 'Accept: application/vnd.btwb.v1.webwidgets+json' -H 'Accept-Language: de,en-US;q=0.7,en;q=0.3' -H 'Accept-Encoding: gzip, deflate, br' -H 'Authorization: Bearer 3yns3r6sc1ytcaogxw7g6tfrr' -H 'Origin: https://static.asvz.ch' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://static.asvz.ch/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: cross-site' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: trailers' -o " + jsonpath)
 browser.implicitly_wait(100)
 browser.get("https://static.asvz.ch/ASVZ-Crossfit/CF-WOD.html")
 browser.implicitly_wait(10000)
